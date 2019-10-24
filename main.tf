2cebb4273577a03a05a643cf1ba09808c832c127
Paul Thrasher <thrashr888@gmail.com>
2019-10-24 16:48:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,55 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_virtual_machine" "primary" {
+  name                  = "${var.prefix}-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "${var.vm_size}"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "${var.prefix}-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "${var.username}"
+    admin_password = "${var.password}"
+
+    custom_data = "echo 'init test'"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      key_data = "${var.public_key}"
+      path = "/home/${var.username}/.ssh/authorized_keys"
+    }
+  }
+  tags = {
+    environment = "test"
+    owner = "thrashr888"
+    organization = "hashicorp"
+    application = "example"
+  }
+}
+
+resource "azurerm_virtual_machine" "secondary" {
   name                  = "${var.prefix}-vm"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
